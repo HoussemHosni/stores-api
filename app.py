1ebc5fa05cf89f07f74b4e5f07f1311eee6eeb4b
HoussemHosni <houssem.hosni12@outlook.fr>
2021-07-17 13:33:32 +0200

    fix bugs

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from datetime import timedelta
 from db import db
 from flask import Flask
@@ -8,25 +9,27 @@ from resources.store import Store, StoreList
 from resources.user import UserRegister
 from security import authenticate, identity
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.data'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "secret_key"
 app.config['JWT_AUTH_URL_RULE'] = '/login'
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 
+db.init_app(app)
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
-db.init_app(app)
-
 @app.before_first_request
 def create_tables():
-    try:
-        db.create_all()
-    except Exception as e:
-        print(e)
+    db.create_all()
+
+@app.route('/')
+def home():
+    return "Hello World!"
+    
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(Store, '/store/<string:name>')
@@ -34,4 +37,4 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/signup')
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(debug=True)
